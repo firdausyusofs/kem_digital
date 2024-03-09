0e75a694d796f2725bdda86a59cec3b208532a9e
Firdaus Yusof <me@firdausyusof.com>
2024-03-09 12:23:23 +0800

    implemented config using viper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"firdausyusofs/kem_digital/config"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load config from config.yml
+	cfgFile, err := config.LoadConfig("./config/config")
+	if err != nil {
+		log.Fatalf("Error loading config file: %s", err)
+	}
+
+	// Parse config into Config struct
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("Error parsing config file: %s", err)
+	}
+
+	log.Printf("Config: %+v", cfg)
 }
---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"errors"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig
+}
+
+type ServerConfig struct {
+	Port string
+}
+
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("Config file not found")
+		}
+	}
+
+	return v, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
